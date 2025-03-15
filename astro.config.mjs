42d004af91a0ec775c709a0660432a728b0d37a4
Jeremy Herzog <jeremyaherzog@gmail.com>
2025-03-15 15:12:58 -0400

    feat: add deployment pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fulton-ring.github.io',
+  base: 'landing-page',
   vite: {
     plugins: [tailwindcss()],
   },
